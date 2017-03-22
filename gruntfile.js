940a43a9690ddb2c7c17d3a7281144878396f2a1
blinklv <blinklv@icloud.com>
2017-03-22 21:46:24 +0800

    Initial project.

---
+++ gruntfile.js
@@ -0,0 +1,17 @@
+// gruntfile.js
+//
+// Author: blinklv <blinklv@icloud.com>
+// Create Time: 2017-03-22
+// Maintainer: blinklv <blinklv@icloud.com>
+// Last Change: 2017-03-22
+// Purpose: The gruntfile.js for Web development.
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+    });
+};
+
+
+
+
