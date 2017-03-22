e87316d5aa5296af5e7b5a7f56f27612e785c78a
blinklv <blinklv@icloud.com>
2017-03-22 22:31:25 +0800

    Add cssmin module.

--- gruntfile.js
+++ gruntfile.js
@@ -9,7 +9,22 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
+
+        // Minifying CSS files.
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: "css/",
+                    src: ["*.css", "!*.min.css"],
+                    dest: "css/",
+                    ext: ".min.css"
+                }]
+            }
+        }
     });
+
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
 };
 
 
